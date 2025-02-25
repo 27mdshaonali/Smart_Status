c37583ba22b5eb8caac6dd66cddfb7687e2746cd
ShAoN <27mdshaonali@gmail.com>
2025-02-25 06:29:11 +0600

    Smart_Status

---
+++ app/src/main/java/com/example/sstatus/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.sstatus;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import soup.neumorphism.NeumorphTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String dashboard_url = "https://codecanvas.top/Smart%20Status/JSon/Dashboard%20Titles%20and%20Images.json";
+    String status_url = "https://codecanvas.top/Smart%20Status/JSon/All%20Status.json";
+    JSONArray STATUS_RESPONSE;
+    ArrayList<HashMap<String, String>> dashboardList = new ArrayList<>();
+    ArrayList<HashMap<String, String>> statusList = new ArrayList<>();
+    GridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initializeView();
+    }
+
+    public void initializeView() {
+        gridView = findViewById(R.id.gridView);
+        parseData();
+    }
+
+    public void parseData() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, dashboard_url, null, response -> {
+            try {
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject jsonObject = response.getJSONObject(i);
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("type", jsonObject.getString("type"));
+                    hashMap.put("dashboardItemImg", jsonObject.getString("icon"));
+                    hashMap.put("dashboardItemTle", jsonObject.getString("title"));
+                    dashboardList.add(hashMap);
+                }
+
+                MyAdapter myAdapter = new MyAdapter(dashboardList);
+                gridView.setAdapter(myAdapter);
+
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Dashboard JSON Parsing Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }, error -> Toast.makeText(MainActivity.this, "Dashboard Error", Toast.LENGTH_SHORT).show());
+
+        JsonArrayRequest arrayRequest2 = new JsonArrayRequest(Request.Method.GET, status_url, null, response -> {
+            try {
+                for (int x = 0; x < response.length(); x++) {
+                    JSONObject jsonObject = response.getJSONObject(x);
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("title2", jsonObject.getString("title"));
+                    hashMap.put("content", jsonObject.getString("content"));
+                    statusList.add(hashMap);
+
+                    STATUS_RESPONSE = response;
+                }
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Status JSON Parsing Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }, error -> Toast.makeText(MainActivity.this, "Status Error", Toast.LENGTH_SHORT).show());
+
+        requestQueue.add(arrayRequest);
+        requestQueue.add(arrayRequest2);
+
+    }
+
+    public class MyAdapter extends BaseAdapter {
+        ArrayList<HashMap<String, String>> dataList;
+
+        public MyAdapter(ArrayList<HashMap<String, String>> list) {
+            this.dataList = list;
+        }
+
+        @Override
+        public int getCount() {
+            return dataList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return dataList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            LayoutInflater layoutInflater = getLayoutInflater();
+            @SuppressLint("ViewHolder") View myView = layoutInflater.inflate(R.layout.dashboard_item_layout, viewGroup, false);
+
+            RoundedImageView imageView = myView.findViewById(R.id.imageView1);
+            NeumorphTextView textView = myView.findViewById(R.id.dashboardItemTittle);
+
+            LinearLayout linearLayout = myView.findViewById(R.id.cardLinear);
+
+            HashMap<String, String> hashMap = dataList.get(i);
+
+            String type = hashMap.get("type");
+            if (type.equals("dashboard")) {
+                textView.setText(hashMap.get("dashboardItemTle"));
+                Picasso.get().load(hashMap.get("dashboardItemImg")).into(imageView);
+            }
+
+
+            linearLayout.setOnClickListener(v -> {
+
+                if (STATUS_RESPONSE != null) {
+
+                    String dashTitle = hashMap.get("dashboardItemTle");
+
+                    Status.RESPONSE = STATUS_RESPONSE;
+                    Status.DASHBOARD_TITLE = dashTitle;
+                    startActivity(new Intent(MainActivity.this, Status.class));
+
+                }
+
+            });
+
+            return myView;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sstatus/Status.java
@@ -0,0 +1,150 @@
+package com.example.sstatus;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Status extends AppCompatActivity {
+
+    // Static Variables
+    public static JSONArray RESPONSE = null;
+    public static String DASHBOARD_TITLE = "";
+
+    // UI Elements
+    private ListView listView;
+    private MyAdapter myAdapter;
+
+    // Data List
+    private final ArrayList<HashMap<String, String>> statusList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_status);
+
+        // Adjust Layout for System Bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initializeView();
+    }
+
+    private void initializeView() {
+        listView = findViewById(R.id.listView);
+        myAdapter = new MyAdapter();
+        listView.setAdapter(myAdapter);
+        parseData();
+    }
+
+    private void parseData() {
+        if (RESPONSE == null) {
+            Toast.makeText(this, "Response is null", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            statusList.clear(); // Clear previous data
+
+            for (int i = 0; i < RESPONSE.length(); i++) {
+                JSONObject jsonObject = RESPONSE.getJSONObject(i);
+
+                String title = jsonObject.getString("title");
+                String content = jsonObject.getString("content");
+                String type = jsonObject.getString("type");
+                String quotes_bg = jsonObject.getString("bg");
+
+//                // Filtering data based on dashboard title
+//                if ((DASHBOARD_TITLE.equalsIgnoreCase("Sad") && type.equalsIgnoreCase("love")) ||
+//                        (DASHBOARD_TITLE.equalsIgnoreCase("Motivational Quotes") && type.equalsIgnoreCase("sad"))) {
+//                    HashMap<String, String> itemMap = new HashMap<>();
+//                    itemMap.put("type", type);
+//                    itemMap.put("title", title);
+//                    itemMap.put("content", content);
+//                    statusList.add(itemMap);
+//                }
+
+
+                // Filtering data based on dashboard title
+                if (DASHBOARD_TITLE.equalsIgnoreCase(type)) {
+                    HashMap<String, String> itemMap = new HashMap<>();
+                    itemMap.put("type", type);
+                    itemMap.put("title", title);
+                    itemMap.put("content", content);
+                    itemMap.put("bg", quotes_bg);
+                    statusList.add(itemMap);
+                }
+
+
+
+            }
+
+            Toast.makeText(this, "Dashboard Title: " + DASHBOARD_TITLE, Toast.LENGTH_SHORT).show();
+            myAdapter.notifyDataSetChanged(); // Notify adapter about data changes
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "JSON Parsing Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return statusList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return statusList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            LayoutInflater layoutInflater = getLayoutInflater();
+            @SuppressLint("ViewHolder")
+            View myView = layoutInflater.inflate(R.layout.content_status, viewGroup, false);
+
+            // UI Elements
+            ImageView imageView = myView.findViewById(R.id.contentImageView);
+            TextView textView = myView.findViewById(R.id.contentTextView);
+
+            // Setting Data
+            HashMap<String, String> hashMap = statusList.get(i);
+            textView.setText(hashMap.get("content"));
+            Picasso.get().load(hashMap.get("bg")).placeholder(R.drawable.shaon).into(imageView);
+
+            return myView;
+        }
+    }
+}
